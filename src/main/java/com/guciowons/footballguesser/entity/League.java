71fbce337c02af91cd74dd4936cbdd3b3a8c5791
GucioWons <wgutek1@wp.pl>
2022-10-06 18:15:51 +0200

    League entity

---
+++ src/main/java/com/guciowons/footballguesser/entity/League.java
@@ -0,0 +1,13 @@
+package com.guciowons.footballguesser.entity;
+
+public class League {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
