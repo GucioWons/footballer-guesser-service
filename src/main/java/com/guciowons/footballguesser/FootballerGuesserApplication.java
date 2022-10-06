fd10a23b27b8978f3cae7c04efa4ef362e7ea7be
GucioWons <wgutek1@wp.pl>
2022-10-06 18:13:15 +0200

    Initial commit

---
+++ src/main/java/com/guciowons/footballguesser/FootballerGuesserApplication.java
@@ -0,0 +1,13 @@
+package com.guciowons.footballguesser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballerGuesserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballerGuesserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guciowons/footballguesser/FootballerGuesserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guciowons.footballguesser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballerGuesserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
