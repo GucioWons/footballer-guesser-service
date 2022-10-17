49e1ee75b83c1800b76fed709d05101c6732df43
GucioWons <wgutek1@wp.pl>
2022-10-17 21:33:46 +0200

    Downloading footballers on start of app

--- src/main/java/com/guciowons/footballguesser/footballer/FootballerService.java
+++ src/main/java/com/guciowons/footballguesser/footballer/FootballerService.java
@@ -1,69 +1,28 @@
 package com.guciowons.footballguesser.footballer;
 
-import com.guciowons.footballguesser.Club.ClubService;
-import com.guciowons.footballguesser.External.*;
-import com.guciowons.footballguesser.League.League;
-import com.guciowons.footballguesser.League.LeagueService;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 public class FootballerService {
     private final FootballerRepository footballerRepository;
-    private final ClubService clubService;
-    private final LeagueService leagueService;
-    private final SquadClient squadClient;
-    private final SquadConverter squadConverter;
+    private final FootballerProvider footballerProvider;
 
-    public FootballerService(FootballerRepository footballerRepository, ClubService clubService, LeagueService leagueService, SquadClient squadClient, SquadConverter squadConverter) {
+    public FootballerService(FootballerRepository footballerRepository, FootballerProvider footballerProvider) {
         this.footballerRepository = footballerRepository;
-        this.clubService = clubService;
-        this.leagueService = leagueService;
-        this.squadClient = squadClient;
-        this.squadConverter = squadConverter;
+        this.footballerProvider = footballerProvider;
     }
 
+    @PostConstruct
+    public void getFootballersFromExternalApi(){
+        footballerProvider.getFootballers(Arrays.asList("BL1", "PL"));
+    }
 
     public List<Footballer> getFootballers(){
-        AtomicInteger counter = new AtomicInteger(0);
-        Arrays.asList("BL1", "PL").forEach(leagueCode ->{
-            Teams teams = getTeamsFromLeague(counter, leagueCode);
-            League league = leagueService.createLeague(teams.getCompetition());
-            teams.getTeams().forEach(team -> getPlayersAndSave(counter, team, league));
-        });
         return footballerRepository.findAll();
-    }
-
-    private Teams getTeamsFromLeague(AtomicInteger counter, String leagueCode){
-        checkCounter(counter);
-        return squadClient.getTeams("5740d267c15143f5b1ab75b03ffce4a3", leagueCode);
-    }
-
-    private void getPlayersAndSave(AtomicInteger counter, Teams.Team team, League league){
-        checkCounter(counter);
-        footballerRepository.saveAll(getPlayersFromTeam(team, league));
-    }
-
-    private List<Footballer> getPlayersFromTeam(Teams.Team team, League league) {
-        return squadConverter.convertSquadToFootballers(
-                squadClient.getSquad("5740d267c15143f5b1ab75b03ffce4a3", team.getId()),
-                clubService.createClub(team, league));
-    }
-
-    private void checkCounter(AtomicInteger counter) {
-        System.out.println(counter.get());
-        if (counter.get() % 10 == 0 && counter.get() != 0) {
-            try {
-                TimeUnit.MINUTES.sleep(1);
-            }catch(InterruptedException e){
-                System.out.println(e.getMessage());
-            }
-        }
-        counter.incrementAndGet();
     }
 
     public List<Footballer> getFootballersByClub(Integer clubId) {
