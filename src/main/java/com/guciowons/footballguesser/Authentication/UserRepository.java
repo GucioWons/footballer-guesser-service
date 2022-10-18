da8b4f4ebfbd6fd74c632bea9c732cb6711593fd
GucioWons <wgutek1@wp.pl>
2022-10-18 22:06:30 +0200

    User repository

---
+++ src/main/java/com/guciowons/footballguesser/Authentication/UserRepository.java
@@ -0,0 +1,8 @@
+package com.guciowons.footballguesser.Authentication;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
